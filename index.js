bf16c506a584191a6075df0f4509516aafc53d3c
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-05-29 18:08:03 -0500

    Usando metodo get

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Saludos desde express');
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
+
 // const express = require("express");
 // const app = express();
-
-// app.get('/', function (req, res) {
-//     res.send('Saludos desde express');
-//   });
-
+// app.post('/hola', function (req, res) {
+//   res.send('[POST]Saludos desde express');
+// });
+// app.get('/hola', function (req, res) {
+//   res.send('[GET]Saludos desde express');
+// });
 // app.listen(3000, () => {
 //  console.log("El servidor está inicializado en el puerto 3000");
 // });
-
-const express = require("express");
-const app = express();
-app.post('/hola', function (req, res) {
-  res.send('[POST]Saludos desde express');
-});
-app.get('/hola', function (req, res) {
-  res.send('[GET]Saludos desde express');
-});
-app.listen(3000, () => {
- console.log("El servidor está inicializado en el puerto 3000");
-});
